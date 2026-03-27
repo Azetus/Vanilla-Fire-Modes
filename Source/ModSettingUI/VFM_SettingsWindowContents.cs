0cae09350d39caea9aaf2b44d8d038866d3011c5
Azetus <izana723@hotmail.com>
2026-03-27 20:37:07 +0800

    优化Mod设置的tab页，改用原版组件

--- Source/ModSettingUI/VFM_SettingsWindowContents.cs
+++ Source/ModSettingUI/VFM_SettingsWindowContents.cs
@@ -9,62 +9,46 @@ namespace VFM_VanillaFireModes.ModSettingUI
 {
     internal static class VFM_SettingsWindowContents
     {
-        private enum TacticTab { AutoSelectionTab, PrecisionTab, BurstTab, SuppressionTab, DefaultTab }
+        private enum TacticTab
+        {
+            AutoSelectionTab,
+            PrecisionTab,
+            BurstTab,
+            SuppressionTab,
+            DefaultTab
+        }
+
         private static TacticTab currentTab = TacticTab.AutoSelectionTab;
 
         private static Vector2 scrollPos;
         private static float lastCalculatedHeight = 1000f;
-        public static void SettingsWindowContents(Rect inRect,ref VanillaFireModesModSetting settings)
+
+        public static void SettingsWindowContents(Rect inRect, ref VanillaFireModesModSetting settings)
         {
-
-            float tabHeight = 30f;
-            float tabWidth = inRect.width / 5f;
-
+            GUI.BeginGroup(inRect);
+            Rect tabtop = new Rect(0, TabDrawer.TabHeight, inRect.width, 0);
+            List<TabRecord> tablist = new List<TabRecord>();
             // Auto Selection Tab
-            GUI.color = (currentTab == TacticTab.AutoSelectionTab) ? Color.yellow : Color.white;
-            if (Widgets.ButtonText(new Rect(inRect.x, inRect.y, tabWidth, tabHeight), "VFM_AutoSelection_Label".Translate()))
-            {
-                currentTab = TacticTab.AutoSelectionTab;
-                SoundDefOf.Tick_High.PlayOneShotOnCamera();
-            }
-
+            tablist.Add(new TabRecord("VFM_AutoSelection_Label".Translate(), () => { currentTab = TacticTab.AutoSelectionTab; },
+                currentTab == TacticTab.AutoSelectionTab));
             // Precision Tab
-            GUI.color = (currentTab == TacticTab.PrecisionTab) ? Color.yellow : Color.white;
-            if (Widgets.ButtonText(new Rect(inRect.x + tabWidth, inRect.y, tabWidth, tabHeight), "VFM_PrecisionMode".Translate()))
-            {
-                currentTab = TacticTab.PrecisionTab;
-                SoundDefOf.Tick_High.PlayOneShotOnCamera();
-            }
-
+            tablist.Add(new TabRecord("VFM_PrecisionMode".Translate(), () => { currentTab = TacticTab.PrecisionTab; },
+                currentTab == TacticTab.PrecisionTab));
             // Burst Tab
-            GUI.color = (currentTab == TacticTab.BurstTab) ? Color.yellow : Color.white;
-            if (Widgets.ButtonText(new Rect(inRect.x + tabWidth * 2, inRect.y, tabWidth, tabHeight), "VFM_ShortBurstMode".Translate()))
-            {
-                currentTab = TacticTab.BurstTab;
-                SoundDefOf.Tick_High.PlayOneShotOnCamera();
-            }
-
+            tablist.Add(new TabRecord("VFM_ShortBurstMode".Translate(), () => { currentTab = TacticTab.BurstTab; },
+                currentTab == TacticTab.BurstTab));
             // Suppression Tab
-            GUI.color = (currentTab == TacticTab.SuppressionTab) ? Color.yellow : Color.white;
-            if (Widgets.ButtonText(new Rect(inRect.x + tabWidth * 3, inRect.y, tabWidth, tabHeight), "VFM_SuppressionMode".Translate()))
-            {
-                currentTab = TacticTab.SuppressionTab;
-                SoundDefOf.Tick_High.PlayOneShotOnCamera();
-            }
-            GUI.color = Color.white;
-            
+            tablist.Add(new TabRecord("VFM_SuppressionMode".Translate(), () => { currentTab = TacticTab.SuppressionTab; },
+                currentTab == TacticTab.SuppressionTab));
             // Default Tab
-            GUI.color = (currentTab == TacticTab.DefaultTab) ? Color.yellow : Color.white;
-            if (Widgets.ButtonText(new Rect(inRect.x + tabWidth * 4, inRect.y, tabWidth, tabHeight), "VFM_DefaultMode".Translate()))
-            {
-                currentTab = TacticTab.DefaultTab;
-                SoundDefOf.Tick_High.PlayOneShotOnCamera();
-            }
-            GUI.color = Color.white;
+            tablist.Add(new TabRecord("VFM_DefaultMode".Translate(), () => { currentTab = TacticTab.DefaultTab; },
+                currentTab == TacticTab.DefaultTab));
+            TabDrawer.DrawTabs(tabtop, tablist);
+
 
             float footerHeight = 45f;
-            Rect scrollOutRect = new Rect(inRect.x, inRect.y + tabHeight + 10f, inRect.width, inRect.height - tabHeight - footerHeight - 15f);
-            //Rect viewRect = new Rect(0f, 0f, inRect.width - 24f, lastCalculatedHeight);
+            float headerHeight = TabDrawer.TabHeight + 10f;
+            Rect scrollOutRect = new Rect(0, headerHeight, inRect.width, inRect.height - headerHeight - footerHeight);
             Rect viewRect = new Rect(0f, 0f, inRect.width - 24f, lastCalculatedHeight);
             Widgets.BeginScrollView(scrollOutRect, ref scrollPos, viewRect);
             Listing_Standard ls = new Listing_Standard();
@@ -80,7 +64,7 @@ namespace VFM_VanillaFireModes.ModSettingUI
                         ref settings.burstMinDistance,
                         ref settings.precisionMinDistance,
                         ref settings.enableFireModeForNPC
-                        );
+                    );
                     break;
                 case TacticTab.PrecisionTab:
                     // 精确射击
@@ -90,8 +74,12 @@ namespace VFM_VanillaFireModes.ModSettingUI
                         ref settings.precisionBurstOption,
                         ref settings.precisionBurstLinearMultiplier,
                         ref settings.precisionBurstAdditiveBonus,
-                        ref settings.precisionBurstTentMaxMultiplier, ref settings.precisionBurstTentSlopeK, ref settings.precisionBurstTentPeakOffset,
-                        ref settings.precisionBurstAdaptiveBonus, ref settings.precisionBurstAdaptivePeakOffset);
+                        ref settings.precisionBurstTentMaxMultiplier,
+                        ref settings.precisionBurstTentSlopeK,
+                        ref settings.precisionBurstTentPeakOffset,
+                        ref settings.precisionBurstAdaptiveBonus,
+                        ref settings.precisionBurstAdaptivePeakOffset
+                    );
                     break;
                 case TacticTab.BurstTab:
                     // 短点射
@@ -101,8 +89,12 @@ namespace VFM_VanillaFireModes.ModSettingUI
                         ref settings.burstBurstOption,
                         ref settings.burstBurstLinearMultiplier,
                         ref settings.burstBurstAdditiveBonus,
-                        ref settings.burstBurstTentMaxMultiplier, ref settings.burstBurstTentSlopeK, ref settings.burstBurstTentPeakOffset,
-                        ref settings.burstBurstAdaptiveBonus, ref settings.burstBurstAdaptivePeakOffset);
+                        ref settings.burstBurstTentMaxMultiplier,
+                        ref settings.burstBurstTentSlopeK,
+                        ref settings.burstBurstTentPeakOffset,
+                        ref settings.burstBurstAdaptiveBonus,
+                        ref settings.burstBurstAdaptivePeakOffset
+                    );
                     break;
                 case TacticTab.SuppressionTab:
                     // 压制射击
@@ -112,8 +104,12 @@ namespace VFM_VanillaFireModes.ModSettingUI
                         ref settings.suppressionBurstOption,
                         ref settings.suppressionBurstLinearMultiplier,
                         ref settings.suppressionBurstAdditiveBonus,
-                        ref settings.suppressionBurstTentMaxMultiplier, ref settings.suppressionBurstTentSlopeK, ref settings.suppressionBurstTentPeakOffset,
-                        ref settings.suppressionBurstAdaptiveBonus, ref settings.suppressionBurstAdaptivePeakOffset);
+                        ref settings.suppressionBurstTentMaxMultiplier,
+                        ref settings.suppressionBurstTentSlopeK,
+                        ref settings.suppressionBurstTentPeakOffset,
+                        ref settings.suppressionBurstAdaptiveBonus,
+                        ref settings.suppressionBurstAdaptivePeakOffset
+                    );
                     break;
                 case TacticTab.DefaultTab:
                     // 默认模式
@@ -123,12 +119,16 @@ namespace VFM_VanillaFireModes.ModSettingUI
                         ref settings.defaultBurstOption,
                         ref settings.defaultBurstLinearMultiplier,
                         ref settings.defaultBurstAdditiveBonus,
-                        ref settings.defaultBurstTentMaxMultiplier, ref settings.defaultBurstTentSlopeK, ref settings.defaultBurstTentPeakOffset,
-                        ref settings.defaultBurstAdaptiveBonus, ref settings.defaultBurstAdaptivePeakOffset,
-                        "VFM_Default_Warning_Label".Translate(),Color.yellow);
+                        ref settings.defaultBurstTentMaxMultiplier,
+                        ref settings.defaultBurstTentSlopeK,
+                        ref settings.defaultBurstTentPeakOffset,
+                        ref settings.defaultBurstAdaptiveBonus,
+                        ref settings.defaultBurstAdaptivePeakOffset,
+                        "VFM_Default_Warning_Label".Translate(),
+                        Color.yellow
+                    );
                     break;
             }
-
 
 
             lastCalculatedHeight = ls.CurHeight + 20f;
@@ -136,20 +136,21 @@ namespace VFM_VanillaFireModes.ModSettingUI
             Widgets.EndScrollView();
 
 
-            Rect footerRect = new Rect(inRect.x, inRect.yMax - footerHeight + 5f, inRect.width, footerHeight);
+            Rect footerRect = new Rect(0, scrollOutRect.yMax + 10f, inRect.width, footerHeight - 15f);
 
             GUI.color = new Color(1f, 1f, 1f, 0.3f);
             Widgets.DrawLineHorizontal(footerRect.x, footerRect.y, footerRect.width);
             GUI.color = Color.white;
             // Reset button
-            Rect resetRect = new Rect(footerRect.xMax - 240f, footerRect.y + 5f, 240f, 30f); ;
+            Rect resetRect = new Rect(footerRect.xMax - 240f, footerRect.y + 5f, 240f, 30f);
             if (Widgets.ButtonText(resetRect, "VFM_ResetButton_Label".Translate()))
             {
                 settings.ResetSetting();
                 SoundDefOf.Tick_High.PlayOneShotOnCamera();
             }
-            GUI.color = Color.white;
 
+            GUI.color = Color.white;
+            GUI.EndGroup();
         }
     }
 }
