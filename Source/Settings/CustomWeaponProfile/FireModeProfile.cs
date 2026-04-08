a2aa7308e186fad495c5bee4b909c49970ca8c5a
Azetus <izana723@hotmail.com>
2026-04-08 19:42:30 +0800

    FireModeProfile

---
+++ Source/Settings/CustomWeaponProfile/FireModeProfile.cs
@@ -0,0 +1,38 @@
+﻿using Verse;
+
+namespace VFM_VanillaFireModes.Settings.CustomWeaponProfile;
+
+public class FireModeProfile : IExposable
+{
+    public float accuracyMultiplier = 1.0f;
+    public float warmupMultiplier = 1.0f;
+    public float cooldownMultiplier = 1.0f;
+    public int burstShotCount = 1;
+
+    public FireModeProfile(
+        float accuracyMultiplier = 1.0f,
+        float warmupMultiplier = 1.0f,
+        float cooldownMultiplier = 1.0f,
+        int burstShotCount = 1)
+    {
+        this.accuracyMultiplier = accuracyMultiplier;
+        this.warmupMultiplier = warmupMultiplier;
+        this.cooldownMultiplier = cooldownMultiplier;
+        this.burstShotCount = burstShotCount;
+    }
+
+    public void ExposeData()
+    {
+        Scribe_Values.Look(ref accuracyMultiplier, "accuracyMultiplier", 1.0f);
+        Scribe_Values.Look(ref warmupMultiplier, "warmupMultiplier", 1.0f);
+        Scribe_Values.Look(ref cooldownMultiplier, "cooldownMultiplier", 1.0f);
+        Scribe_Values.Look(ref burstShotCount, "burstShotCount", 1);
+        if (Scribe.mode == LoadSaveMode.PostLoadInit)
+        {
+            if (accuracyMultiplier <= 0) accuracyMultiplier = 1.0f;
+            if (warmupMultiplier <= 0) warmupMultiplier = 1.0f;
+            if (cooldownMultiplier <= 0) cooldownMultiplier = 1.0f;
+            if (burstShotCount < 1) burstShotCount = 1;
+        }
+    }
+}
---
+++ Source/Settings/CustomWeaponProfile/WeaponFireModeProfile.cs
@@ -0,0 +1,40 @@
+﻿using Verse;
+
+namespace VFM_VanillaFireModes.Settings.CustomWeaponProfile;
+
+public class WeaponFireModeProfile : IExposable
+{
+    public string defName;
+
+    public FireModeProfile Default;
+    public FireModeProfile Precision;
+    public FireModeProfile Burst;
+    public FireModeProfile Suppression;
+
+    public WeaponFireModeProfile(string defName)
+    {
+        this.defName = defName;
+    }
+
+    public bool defIsValid()
+    {
+        return !defName.NullOrEmpty() &&
+               DefDatabase<ThingDef>.GetNamedSilentFail(defName) != null;
+    }
+
+    public void ExposeData()
+    {
+        Scribe_Values.Look(ref defName, "defName");
+        Scribe_Deep.Look(ref Default, "Default", new FireModeProfile());
+        Scribe_Deep.Look(ref Precision, "Precision", new FireModeProfile());
+        Scribe_Deep.Look(ref Burst, "Burst", new FireModeProfile());
+        Scribe_Deep.Look(ref Suppression, "Suppression", new FireModeProfile());
+        if (Scribe.mode == LoadSaveMode.PostLoadInit)
+        {
+            Default ??= new FireModeProfile();
+            Precision ??= new FireModeProfile();
+            Burst ??= new FireModeProfile();
+            Suppression ??= new FireModeProfile();
+        }
+    }
+}
